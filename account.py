fb2451dfd3a2815a9f48b3b6baa0146ed783f3a5
ChesangFelister <47848763+ChesangFelister@users.noreply.github.com>
2019-05-09 14:50:48 +0300

    python classes example

---
+++ account.py
@@ -0,0 +1,15 @@
+class Account:
+	def __init__(self,first_name,second_name,balance):
+		self.first_name=first_name
+		self.second_name=second_name
+		self.balance=balance
+	def fullname(self):
+		return fullname
+
+	def deposit(self):
+		return balance+30000
+
+	def withdraw(self):
+		return" sorry you can not withdraw 40000 since you have insufficient balance"
+
+		
