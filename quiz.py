59cd8b5cac9a54450da1315c756aa024dde62be8
ChesangFelister <47848763+ChesangFelister@users.noreply.github.com>
2019-05-09 14:39:52 +0300

    Add files via upload
    first commit

---
+++ quiz.py
@@ -0,0 +1,40 @@
+def tens():
+ 	x=[2,3,4,5,6]
+ 	for number in x:
+ 		print(number*10)
+def lists():
+	
+	square=[number*number for number in range(10)]
+	print(square)
+
+def sorted():
+	l=[2,4,7,9,0]
+	u=[3,9,70,6,7]
+	a=[]
+	a=u+l
+	a.sort()
+	print(a)
+
+def range_sum(n):
+	range_sum=range(1,n+1)
+	final_sum=sum(range_sum)
+	print(final_sum)
+
+
+def large():
+	a=[2,4,7,8,9]
+	print(min(a))
+
+def small():
+	b=[5,7,12,30]
+	print(min(b))
+
+def my_module():
+	x=dict()
+	for a in range(10,20):
+		j[x]=a%3:
+			print(j)
+
+
+		
+
